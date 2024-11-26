c88e68fb3601213eaca9fe9b3a94ad0e2341762c
sourav.1998.usb.sb@gmail.com <sourav.1998.usb.sb@gmail.com>
2024-11-26 19:21:04 +0530

    added

--- src/utils/CTA.jsx
+++ src/utils/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a className='btn' href='https://docs.google.com/document/d/1-E-HQVQ9y-zho7uD-nLpffJq3LyBukMm/edit?usp=sharing&ouid=104390834924619971399&rtpof=true&sd=true' target='_blank'>Download CV</a>
+        <a className='btn' href='https://docs.google.com/document/d/1euM6u_C781CNySZoFrIbuq5yW-S13cfgYkKy8W_hKIo/edit?usp=sharing' target='_blank'>Download CV</a>
         <a className='btn btn-primary' href='#contact'>Let's Talk</a>
     </div>
   )
